cbd015f60387c71f2154178f5d81cbc4784643ab
masterDisaster3 <92509185+masterDisaster3@users.noreply.github.com>
2023-05-24 00:13:54 +0530

    3

--- evaltest.py
+++ evaltest.py
@@ -2,17 +2,7 @@ import streamlit as st
 from streamlit_js_eval import get_geolocation
 import json
 
-
-def clicked():
-    start_tracking = True
-
-
-button1 = st.button("Track", on_click = clicked)
-
-start_tracking = False
-
-
-    
-while start_tracking:
+             
+if st.checkbox("Check my location"):
     loc = get_geolocation()
     st.write(f"Your coordinates are {loc}")
